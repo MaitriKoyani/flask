4b979c7054ab35de4514f16f530032424f7ddb74
MaitriKoyani <program4192@gmail.com>
2024-12-17 18:48:14 +0530

    add feature of addlog,viewlog,update log and delete log

--- app.py
+++ app.py
@@ -1,28 +1,46 @@
-from flask import Flask,render_template,request,make_response,session
+from flask import Flask,render_template,request,make_response,session,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import login_manager,LoginManager
 from sqlalchemy import text
+from datetime import date,time,datetime
 
 app= Flask(__name__)
 bcry= Bcrypt(app)
-app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///datastore.db"
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///Timeline.db"
 app.config["SECRET_KEY"] = "end"
 db=SQLAlchemy(app)
-
-
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 class Detail(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    username=db.Column(db.String(60),unique=True)
-    email=db.Column(db.String,unique=True)
-    password=db.Column(db.String(255),nullable=False)
-    
+    __tablename__ = 'detail'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60), unique=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+
+    # Relationship with logs
+    logs = db.relationship('Log', backref='user', lazy=True)
+
+class Log(db.Model):
+    __tablename__ = 'log'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    timein = db.Column(db.Time, nullable=False)
+    timeout = db.Column(db.Time, nullable=False)
+    task = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    hours = db.Column(db.Float, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('detail.id'), nullable=False, index=True)
+
+    # Unique constraint for one log per date per user
+    __table_args__ = (db.UniqueConstraint('date', 'user_id', name='unique_date_user'),)
+
+
 with app.app_context():
-    # db.session.execute(text('DROP TABLE IF EXISTS data1;'))
+    # db.session.execute(text('DROP TABLE IF EXISTS datastore;'))
     # db.session.commit()
     # print("Table deleted successfully.")
 
@@ -93,7 +111,11 @@ def login():
                 
                         msg=' login successful'
                         session['username']=user.username
+                        
                         return render_template('welcome.html',username=user.username,msg=msg)
+                    else:
+                        msg='Wrong password!'
+                        return render_template('login.html',msg=msg)
                 except Exception as e:
                     msg='Wrong password!'
                     return render_template('login.html',msg=msg)
@@ -103,19 +125,82 @@ def login():
             return render_template('login.html',msg=msg)
         else:
             return render_template('login.html',msg=msg)
-@app.route("/logout/<int:id>", methods=['POST','GET'])
-def logout(id):
-    user = db.get_or_404(Detail, id)
+@app.route("/logout")
+def logout():
     msg=''
-    if request.method == "POST":
-        
-        session[user.username] = None
+    if 'username' in session:
         session.pop('username', None)
-        
-        msg='This user '+user.username+' is logout!'
+        msg='This user was logout!'
         return render_template('index.html',msg=msg)
-
-    return render_template("logout.html", msg=msg,username='none')
+    else:
+        msg='User isn\'t logged in!'
+        return render_template('index.html',msg=msg)
     
+
+@app.route('/addlog', methods=['POST','GET'])
+def addlog():
+    msg=''
+    if 'username' in session:
+        if request.method == 'POST':
+            log=Log.query.filter_by(date=request.form['date']).first()
+            if log:
+                msg='The log of this date is added , Please change the date!'
+                return render_template('addlog.html', msg=msg)
+            else:
+                user=Detail.query.filter_by(username=session['username']).first()
+                
+                log=Log(date=datetime.strptime(request.form['date'],'%Y-%m-%d').date(),timein=datetime.strptime(request.form['timein'],'%H:%M').time(),timeout=datetime.strptime(request.form['timeout'],'%H:%M').time(),task=request.form['task'],description=request.form['description'],hours=float(request.form['hours']),user_id=user.id)
+                db.session.add(log)
+                db.session.commit()
+                msg='Log added successfully'
+                return redirect(url_for('viewlog'))
+        log=''
+        return render_template('addlog.html',username=session['username'],l=log)
+    else:
+        return redirect(url_for('login'))
+
+    
+@app.route('/viewlog',methods=['GET'])
+def viewlog():
+    if 'username' in session:
+        user=Detail.query.filter_by(username=session['username']).first()
+        us=user.id
+        log=Log.query.filter_by(user_id=us).all()
+        
+        return render_template('viewlog.html',log=log,username=user.username,coun=len(log))
+    else:
+        return redirect(url_for('login'))
+@app.route('/updatelog/<int:id>',methods=['GET','POST'])
+def updatelog(id):
+    log=Log.query.filter_by(id=id).first()
+    user=Detail.query.filter_by(id=log.user_id).first()
+    if request.method == 'POST':
+        log.date=datetime.strptime(request.form['date'],'%Y-%m-%d').date()
+        try:
+            log.timein=datetime.strptime(request.form['timein'],'%H:%M:%S').time()
+        except ValueError:
+            log.timein=datetime.strptime(request.form['timein'],'%H:%M').time()
+        try:
+            log.timeout=datetime.strptime(request.form['timeout'],'%H:%M:%S').time()
+        except ValueError:
+            log.timeout=datetime.strptime(request.form['timeout'],'%H:%M').time()
+
+        log.task=request.form['task']
+        log.description=request.form['description']
+        log.hours=float(request.form['hours'])
+        log.user_id=log.user_id
+        db.session.commit()
+        msg='Log added successfully'
+        return redirect(url_for('viewlog'))
+    return render_template('addlog.html',l=log,username=user.username)
+
+@app.route('/deletelog/<int:id>',methods=['GET'])
+def deletelog(id):
+    log=Log.query.filter_by(id=id).first()
+    db.session.delete(log)
+    db.session.commit()
+    print('log deleted successfully')
+    return redirect(url_for('viewlog'))
+
 if __name__ == '__main__':
     app.run(debug=True) 
